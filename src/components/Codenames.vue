393810a30ad13458dc7baab3f5362b778950f5ef
XxX-MLGNoob-XxX <thomason.isaiah@yahoo.com>
2020-06-01 20:45:09 -0400

    Remove Unnecessary Styles from "Codenames.vue" component

--- src/components/Codenames.vue
+++ src/components/Codenames.vue
@@ -63,45 +63,4 @@ export default Vue.extend({
   justify-self: center;
   align-items: center;
 }
-
-.card {
-  width: 7rem;
-  height: 5rem;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  background: white;
-  border: 2px solid #42424242;
-  border-radius: 5px;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-  position: relative;
-  transition: all 0.3s linear;
-  text-align: left;
-  overflow-wrap: break-word;
-}
-
-.card:hover {
-  transform: scale(1.25, 1.25);
-}
-
-.card::after {
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-
-  border-radius: 5px;
-  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
-  opacity: 0;
-  position: absolute;
-  transition: all 0.3s linear;
-}
-
-.card:hover::after {
-  opacity: 1;
-}
 </style>
